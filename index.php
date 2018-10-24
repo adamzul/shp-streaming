3eb91a4b08aed7d78cac04cad61d94218964771b
ADMIN-PC\ADMIN <adamzulkarnain98@gmail.com>
2018-10-24 22:32:01 +0700

    update

--- index.php
+++ index.php
@@ -6,13 +6,15 @@ $app = new Slim\App();
 $container = $app->getContainer();
 $container['renderer'] = new PhpRenderer("./view");
 
+$app->get('/view', function ($request, $response, $args) {
+    return $this->renderer->render($response, "view.html");
+});
+
 $app->get('/hello/{name}', function ($request, $response, $args) {
     return $response->getBody()->write("Hello, " . $args['name']);
 });
 
-$app->get('/view', function ($request, $response, $args) {
-    return $this->renderer->render($response, "view.html");
-});
+
 
 $app->get('/api/{param}', function ($request, $response, $args) {
 	$str = file_get_contents('./'.$args['param'].'.geojson');
